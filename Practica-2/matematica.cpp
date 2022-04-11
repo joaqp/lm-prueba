8317536bc2182f59b1679b5f86089339d17e2218
Joaquina <joaquipisani@gmail.com>
2022-04-11 17:09:27 -0300

    Actualizo ejercicios y agrego el 5

--- Practica-2/matematica.cpp
+++ Practica-2/matematica.cpp
@@ -3,19 +3,36 @@
 
 using namespace std;
 
-
 int main (void)
 {
 
 int16_t array[5]={1, 1, 1 , 1, 1};
 int n= 5;
 cout << Sumar_Array (array, n) << endl;
+cout << Cuenta_Accesos() << endl;
+cout << Cuenta_Accesos() << endl;
+
 }
 
 int32_t Sumar_Array (int16_t* x, int16_t xn)
 {
-int32_t sum=0;
-for(int i=0; i< xn; i++)
-sum+=x[i];
-return sum;
+ int32_t sum=0;
+ for(int i=0; i< xn; i++)
+  sum+=x[i];
+ return sum;
+}
+
+int16_t Multiplicar_Sat (int16_t n1, int16_t n2)
+{
+
+
+
+
+}
+
+
+uint16_t Cuenta_Accesos (void)
+{
+N++;
+return N;
 }
--- Practica-2/matematica.h
+++ Practica-2/matematica.h
@@ -1,3 +1,5 @@
 #include <iostream>
-
+extern int N=0;
 int32_t Sumar_Array (int16_t* x, int16_t xn);
+int16_t Multiplicar_Sat (int16_t n1, int16_t n2);
+uint16_t Cuenta_Accesos (void);
